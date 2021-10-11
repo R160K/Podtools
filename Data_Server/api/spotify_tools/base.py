25c07d6b33cb75f2525a19f40e5c0f9ab137580b
R160K <R160K@AOL.com>
2021-10-11 23:26:10 +0100

    Deleted accidentally uploaded client secret for Spotify

--- Data_Server/api/spotify_tools/base.py
+++ Data_Server/api/spotify_tools/base.py
@@ -8,8 +8,4 @@ redirect_uri = "http://127.0.0.1"
 authorise_url = "https://accounts.spotify.com/authorize"
 token_url = "https://accounts.spotify.com/api/token"
 
-#DO NOT INCLUDE CLIENT SECRET IN GITHUB UPLOADS
-# f = open("./api/spotify_tools/client_secret.txt", "r")
-# client_secret = f.read()
-# f.close()
-client_secret = "8114a3b8ad104a11a7d2658d243e5ab2"
+client_secret = [REDACTED]
