ce82c9eaf4f94bc4ef7fffb4405820e8f9d05d23
R160K <R160K@AOL.com>
2021-11-10 02:13:43 +0000

    Create app.py

---
+++ blah/app.py
@@ -0,0 +1,7 @@
+from aiohttp import web
+
+async def index(request):
+    return web.Response(text="blah blah blah", content_type="text/html")
+
+app = web.Application()
+app.add_routes([web.get("/", index)])
