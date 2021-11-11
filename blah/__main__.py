4c69d9b9ce01c1f7ea530fbd17fc925f55ad3459
R160K <R160K@AOL.com>
2021-11-11 04:28:06 +0000

    Commit

--- blah/__main__.py
+++
@@ -1,7 +0,0 @@
-from aiohttp import web
-
-async def index(request):
-    return web.Response(text="blah blah blah blah blah", content_type="text/html")
-
-app = web.Application()
-app.add_routes([web.get("/", index)])
